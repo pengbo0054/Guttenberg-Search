a36c6345e79a1aa2321b6afd45b98af7d0b66524
pengbo0054 <pengbo0054@gmail.com>
2018-10-23 22:49:00 +0800

    add esdata

--- server/connection.js
+++ server/connection.js
@@ -1,8 +1,8 @@
 const elasticsearch = require('elasticsearch')
 
 // Core ES variables for this project
-const index = 'library'
-const type = 'book'
+const index = 'cngov'
+const type = 'items'
 const port = 9200
 const host = process.env.ES_HOST || 'localhost'
 const client = new elasticsearch.Client({ host: { host, port } })
